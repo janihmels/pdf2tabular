a3084e9679886178d9094cc3d8889dc6b9df56bb
1gad12 <69901031+1gad12@users.noreply.github.com>
2022-02-03 19:26:31 +0200

    .

--- ApiPython.py
+++ ApiPython.py
@@ -10,16 +10,19 @@ app.config["DEBUG"] = True
 @app.route('/pdfPublisher', methods=['POST'])
 @cross_origin()
 def PdfPublisher():
-    pdflist = []
-    filename = request.form.get('filename')
-    filepath = request.form.get('path')
-    fullfile = str(filepath)+"/"+str(filename)
-    pdf_text = firstformat.pdf_To_text(fullfile, pages=[0])
-    res = firstformat.CheckPdf(pdf_text, fullfile, pdflist)
-    if res == "Try 3":
-        pdf_text = firstformat.pdf_To_text(fullfile, pages=[2])
+    try:
+        pdflist = []
+        filename = request.form.get('filename')
+        filepath = request.form.get('path')
+        fullfile = str(filepath)+"/"+str(filename)
+        pdf_text = firstformat.pdf_To_text(fullfile, pages=[0])
         res = firstformat.CheckPdf(pdf_text, fullfile, pdflist)
-    return jsonify({"result: ":res})
+        if res == "Try 3":
+            pdf_text = firstformat.pdf_To_text(fullfile, pages=[2])
+            res = firstformat.CheckPdf(pdf_text, fullfile, pdflist)
+        return jsonify({"result: ":res})
+    except FileNotFoundError:
+        return jsonify({"result: ":"Error File Not Found"})
 
 
 if __name__ == "__main__":
