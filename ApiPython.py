58794c7a23ecd70408d4fa054aef022d046e4836
1gad12 <69901031+1gad12@users.noreply.github.com>
2022-02-06 15:39:46 +0200

    .

--- ApiPython.py
+++ ApiPython.py
@@ -1,4 +1,4 @@
-import firstformat
+import subs.PdfIdentification
 import flask
 from flask import request, jsonify
 from flask_cors import cross_origin
@@ -7,24 +7,23 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/pdfPublisher', methods=['POST'])
+@app.route('/pdfIdentification', methods=['POST'])
 @cross_origin()
 def PdfPublisher():
     try:
         pdflist = []
         filename = request.form.get('filename')
         filepath = request.form.get('path')
-        fullfile = str(filepath)+"/"+str(filename)
-        pdf_text = firstformat.pdf_To_text(fullfile, pages=[0])
+        fullfile = str(filepath) + "/" + str(filename)
+        pdf_text = PdfIdentification.pdf_To_text(fullfile, pages=[0])
         res = firstformat.CheckPdf(pdf_text, fullfile, pdflist)
         if res == "Try 3":
-            pdf_text = firstformat.pdf_To_text(fullfile, pages=[2])
-            res = firstformat.CheckPdf(pdf_text, fullfile, pdflist)
-        return jsonify({"result: ":res})
+            pdf_text = PdfIdentification.pdf_To_text(fullfile, pages=[2])
+            res = PdfIdentification.CheckPdf(pdf_text, fullfile, pdflist)
+        return jsonify({"result: ": res})
     except FileNotFoundError:
-        return jsonify({"result: ":"Error File Not Found"})
+        return jsonify({"result: ": "Error File Not Found"})
 
 
 if __name__ == "__main__":
     app.run(port=5100)
-
