be29fc5ceb9d6a9add918c076c9cb9fdc9afbd51
1gad12 <69901031+1gad12@users.noreply.github.com>
2022-02-27 17:16:59 +0200

    .

--- ApiPython.py
+++ ApiPython.py
@@ -1,52 +1,74 @@
-@ -1,3 +1,5 @@
-import os
-
 from subs.PdfIdentification import CheckPdf, PdfIdentifier
 from subs.Pdf_To_Text import pdf_To_text
 from subs.PdfAudit import *
-@ -21,9 +23,9 @@ app.config['MAX_CONTENT_PATH'] = 4194304
+from subs.WixenParser import WixenParser
+from subs.PRSParser import PRSParser
+from subs.CMGParser import CMGParser
+from subs.sql2xlsx import sql2xlsx
+from werkzeug.utils import secure_filename
+
+import flask
+from flask import request, jsonify
+from flask_cors import cross_origin, CORS
+
+app = flask.Flask(__name__)
+cors = CORS(app)
+app.config["DEBUG"] = True
+app.config['UPLOAD_FOLDER'] = "Files"
+app.config['MAX_CONTENT_PATH'] = 4194304
+
+
 class Filesfunc:
     def PdfAudit(self, filepath):
         try:
             return pdfAudit(filepath)
-            return jsonify(pdfAudit(filepath))
         except FileNotFoundError:
             return {"result": "Error File Not Found"}
-            return jsonify({"result": "Error File Not Found"})
 
     def PdfParse(self, filepath):
         pdf_type = PdfIdentifier(filepath)
-@ -37,12 +39,12 @@ class Filesfunc:
+
+        if pdf_type == "PRS":
+            parser = PRSParser(pdf_filepath=filepath)
+        elif pdf_type == "WIXEN":
+            parser = WixenParser(pdf_filepath=filepath)
+        elif pdf_type == "CMG":
+            parser = CMGParser(pdf_filepath=filepath)
         else:
             print(pdf_type)
             # format wasn't found:
             return {"result": "format wasn't found"}
-            return jsonify({"result": "format wasn't found"})
 
         parser.parse()
         parser.save_result(os.getcwd() + "\\Files\\xlss\\1.csv")
 
         return {"Success": "file successfully extracted to {0}".format(filepath)}
-        parser.save_result(os.getcwd() + "\\Files\\xlss\\Thefile.csv")
-        #flask.send_file(,as_attachment=False)
-        return flask.url_for("static",filename=os.getcwd()+"\\Files\\xlss\\Thefile.csv",_external=True)
 
     def Sql2Xlsx(self, filepath):
 
-@ -63,12 +65,12 @@ def upload_file1():
+        # dbname = request.form.get('dbname')
+        # queries = request.form.get('queries')                     =============================here=============================
+        # queries = eval(queries)
+
+        return sql2xlsx(dbname=dbname, queries=queries, output_filename=filepath)
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('index.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file1():
     if request.method == 'POST':
         function = request.form.get("thefunction")
         f = request.files["thefile"]
         f.save("Files/" + secure_filename(f.filename))
-        f.save("Files/" + secure_filename("Thefile.pdf"))
         f.close()
         funcs = Filesfunc()
         value = getattr(funcs, function)(os.getcwd() + "\\Files\\" + f.filename)
 
         return jsonify(value)
-        value = getattr(funcs, function)(os.getcwd() + "\\Files\\Thefile.pdf")
-        print(value)
-        return value
 
 @app.route('/parse', methods=['POST'])
 def parse():
@@ -73,3 +95,4 @@ def parse():
     return jsonify({"Type": pdf_type})
 
 if __name__ == "__main__":
+    app.run(port=5100)
