4769787209dd42ed85494ab082a6907505371b37
‪Ilan Motiei‬‏ <85764577+ilanmotiei@users.noreply.github.com>
2022-03-01 14:46:25 +0200

    Removed the neccesity for path_csv when parsing

--- ApiPython.py
+++ ApiPython.py
@@ -79,7 +79,7 @@ def upload_file1():
 @app.route('/parse', methods=['POST'])
 def parse():
     path_pdf = request.form.get('path_pdf')
-    path_csv= request.form.get('path_csv')
+    path_csv = ' '.join(path_pdf.split()[:-1]) + '.pdf'
 
     pdf_type = PdfIdentifier(path_pdf)
 
